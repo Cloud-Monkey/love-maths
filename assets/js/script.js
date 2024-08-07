1a6ca85d371ab23ce5a9ec92db62da968dfa7c3e
Michael Brown <ironmonkeydaveb@yahoo.co.uk>
2024-08-07 08:50:19 +0000

    add event listener to answer-box for keydown and opened function block

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,10 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        
+    })
+
     runGame("addition");
 
 });
